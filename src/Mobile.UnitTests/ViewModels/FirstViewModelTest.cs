9a72eed2b7230f231e5b58525938224849578dbb
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-10-18 19:44:23 +0300

    Added tests with NUnit

---
+++ src/Mobile.UnitTests/ViewModels/FirstViewModelTest.cs
@@ -0,0 +1,36 @@
+﻿using Mobile.Core.ViewModels;
+using NUnit.Framework;
+
+namespace Mobile.UnitTests.ViewModels
+{
+	[TestFixture]
+	public class FirstViewModelTest : MvxTest
+	{
+		[Test]
+		public void GoCausesNoNavigationAfterChangeHello()
+		{
+			ClearAll();
+
+			var mockNavigation = CreateMockNavigation();
+			var viewModel = new FirstViewModel();
+			var hello = "Test string";
+
+			viewModel.Hello = hello;
+
+			Assert.AreEqual(0, mockNavigation.NavigateRequests.Count);
+		}
+
+		[Test]
+		public void ChangesHello()
+		{
+			ClearAll();
+
+			var viewModel = new FirstViewModel();
+			var hello = "Test string";
+
+			viewModel.Hello = hello;
+
+			Assert.AreEqual(hello, viewModel.Hello);
+		}
+	}
+}
