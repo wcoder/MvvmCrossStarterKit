0d984c9f2f789cd246d2aa18dcb060063d764e4a
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-10-18 19:28:44 +0300

    Added test project

---
+++ src/Mobile.Tests/MvxTest.cs
@@ -0,0 +1,18 @@
+﻿using Cirrious.CrossCore.Core;
+using Cirrious.MvvmCross.Test.Core;
+using Cirrious.MvvmCross.Views;
+using Mobile.Tests.Mocks;
+
+namespace Mobile.Tests
+{
+	public class MvxTest : MvxIoCSupportingTest
+	{
+		protected MockMvxViewDispatcher CreateMockNavigation()
+		{
+			var dispatcher = new MockMvxViewDispatcher();
+			Ioc.RegisterSingleton<IMvxMainThreadDispatcher>(dispatcher);
+			Ioc.RegisterSingleton<IMvxViewDispatcher>(dispatcher);
+			return dispatcher;
+		}
+	}
+}
