a8bad752dfbcb685e52dd57b8da2001816f5b11f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-03-31 14:21:58 +0300

    Added no cacheable http service

---
+++ src/Mobile/Mobile.Services/Common/Http/NoCacheableHttpService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Mobile.Services.Common.Http
+{
+	public class NoCacheableHttpService : BaseHttpService
+	{
+		protected override void PrepareHeaders(HttpClient client, HttpRequestMessage requestMessage, AuthenticationHeaderValue auth = null)
+		{
+			base.PrepareHeaders(client, requestMessage, auth);
+
+			client.DefaultRequestHeaders.Add("If-Modified-Since", DateTime.UtcNow.ToString("r")); //Disable caching
+		}
+	}
+}
+
