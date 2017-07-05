2726e5b7f3931ec966d47922a73ecaaac64060da
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-07-05 18:30:00 +0300

    Update MvvmCross to v.5.0.5

--- src/Mobile/Mobile.UWP/Setup.cs
+++ src/Mobile/Mobile.UWP/Setup.cs
@@ -1,7 +1,7 @@
 using Windows.UI.Xaml.Controls;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Platform.Platform;
-using MvvmCross.WindowsUWP.Platform;
+using MvvmCross.Uwp.Platform;
 
 namespace Mobile.UWP
 {
